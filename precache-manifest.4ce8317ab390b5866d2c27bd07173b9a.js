cc95770f3f83fba862e89d1c87346eb5d0dd8e18
senguyen1011 <seantanhnguyen@gmail.com>
2020-07-02 18:07:25 -0400

    Updates

---
+++ precache-manifest.4ce8317ab390b5866d2c27bd07173b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce5d563bb7e2b2d7f9749437fba0e37",
+    "url": "/state-purity/index.html"
+  },
+  {
+    "revision": "125821c68aef3f9dc5bb",
+    "url": "/state-purity/static/css/main.7280c1ee.chunk.css"
+  },
+  {
+    "revision": "2748dee9dfc95b704c65",
+    "url": "/state-purity/static/js/2.b11db0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/state-purity/static/js/2.b11db0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125821c68aef3f9dc5bb",
+    "url": "/state-purity/static/js/main.cbb23fd2.chunk.js"
+  },
+  {
+    "revision": "d9630de1f2b7aa778689",
+    "url": "/state-purity/static/js/runtime-main.8adba17f.js"
+  }
+]);
